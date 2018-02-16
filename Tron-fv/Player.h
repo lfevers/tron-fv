3600ded1b347de3d5e3e31d34a1d96bd76e58d2b
Unknown <sergiopatohumanes@gmail.com>
2018-02-16 17:35:31 +0100

    Imagen de texturas y movimiento
    1º He añadido una imágen con las texturas que me interesan (una con fondo y otra sin fondo).
    
    2º He programado el movimiento del coche y el rastro que va dejando detrás de él.
    
    PROBLEMAS: Problema con la gestión de memoria, se me ralentiza mucho el programa. 
    Todo esta en el main

--- Tron-fv/Player.h
+++ Tron-fv/Player.h
@@ -10,6 +10,7 @@
  *
  * Created on 14 de febrero de 2018, 12:45
  */
+#include <SFML/Graphics.hpp>
 
 #ifndef PLAYER_H
 #define PLAYER_H
@@ -21,73 +22,74 @@ class Player{
         sf::Sprite sprite;
         sf::Color color;
     public:
-        Player(char []);
+        Player(sf::Color, int, int);
         void movimiento_automatico();
-        void movimiento_controlado();
+        void movimiento_controlado(sf::Event *event,sf::RenderWindow *window);
         sf::Sprite getSprite();
 };
 
-Player::Player(char *r_textura, sf::Color _color, int nx_sprite , int ny_sprite){
+Player::Player(sf::Color _color, int nx_sprite , int ny_sprite){
     x = rand() % W;
     y = rand() % H;
     color = _color;
     
     sf::Texture tex;
-    if (!tex.loadFromFile(*r_textura))
+    if (!tex.loadFromFile("resources/sprites-tron-sf.png"))
     {
         std::cerr << "Error cargando la imagen sprites.png";
         exit(0);
     }
     
-    sprite(tex);
+    sprite = sf::Sprite(tex);
     sprite.setOrigin(32/2,32/2);
     sprite.setTextureRect(sf::IntRect(nx_sprite*32,ny_sprite*32,32,32));
     sprite.setPosition(x,y);
 }
 
-Player::movimiento_controlado(sf::Event event,sf::RenderWindow window){
+void Player::movimiento_controlado(sf::Event *event,sf::RenderWindow *window){
 //Verifico si se pulsa alguna tecla de movimiento
-    switch(event.key.code) {
+    switch(*event->key.code) {
                         
         //Mapeo del cursor
         case sf::Keyboard::Right:
-            sprite.setTextureRect(sf::IntRect(0*75, 2*75, 75, 75));
+            sprite.setTextureRect(sf::IntRect(1*32, 0*32, 32, 32));
             //Escala por defecto
             sprite.setScale(1,1);
             sprite.move(kVel,0);
         break;
 
         case sf::Keyboard::Left:
-            sprite.setTextureRect(sf::IntRect(0*75, 2*75, 75, 75));
+            sprite.setTextureRect(sf::IntRect(1*32, 0*32, 32, 32));
             //Reflejo vertical
             sprite.setScale(-1,1);
             sprite.move(-kVel,0); 
         break;
                         
         case sf::Keyboard::Up:
-            sprite.setTextureRect(sf::IntRect(0*75, 3*75, 75, 75));
+            sprite.setTextureRect(sf::IntRect(0*32, 0*32, 32, 32));
             sprite.move(0,-kVel); 
         break;
                         
         case sf::Keyboard::Down:
-            sprite.setTextureRect(sf::IntRect(0*75, 0*75, 75, 75));
+            sprite.setTextureRect(sf::IntRect(0*32, 0*32, 32, 32));
+            sprite.setScale(1,-1);
             sprite.move(0,kVel); 
         break;
-                        
+                              
                         
         //Tecla ESC para salir
         case sf::Keyboard::Escape:
-            window.close();
+            *window->close();
         break;
                         
         //Cualquier tecla desconocida se imprime por pantalla su código
         default:
-            std::cout << event.key.code << std::endl;
+            std::cout << *event->key.code << std::endl;
         break;                  
     }
 }
 
-Player::getSprite(){
+sf::Sprite Player::getSprite(){
     return sprite;
 }
 
--- Tron-fv/main.cpp
+++ Tron-fv/main.cpp
@@ -1,23 +1,50 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
-#include <string.h>
 
-#define kVel 5
-const int W = 640;
-const int H = 480;
-
-#include "Player.h"
-
-
-
+#define kVel 1
 
 int main()
 {
+    float posx =300;
+    float posy =240;
+    
+    int dirx = 0;
+    int diry = 1;
+    
+    int mapa[600][480];
+    for(int mx = 0; mx < 600;mx++){
+            for(int my = 0; my < 480;my++){
+                mapa[mx][my] = 0;
+            }
+        }
+    
     //Creamos una ventana 
-    sf::RenderWindow window(sf::VideoMode(W, H), "P0. Fundamentos de los Videojuegos. DCCIA");
-
+    sf::RenderWindow window(sf::VideoMode(600, 480), "T.R.O.N");
+    
+    
     //Cargo la imagen donde reside la textura del sprite
-    Player p1("sprites.png",sf::Color::Blue,0,0);
+    sf::Texture tex;
+    if (!tex.loadFromFile("resources/sprites-tron-sf.png"))
+    {
+        std::cerr << "Error cargando la imagen sprites.png";
+        exit(0);
+    }
+    
+    sf::Texture tex2;
+    tex2.loadFromFile("resources/background.jpg");
+    
+    
+    //Y creo el spritesheet a partir de la imagen anterior
+    sf::Sprite sprite(tex);
+    sf::Sprite s_background(tex2);
+    
+    //Le pongo el centroide donde corresponde
+    sprite.setOrigin(32/2,32/2);
+    //Cojo el sprite que me interesa por defecto del sheet
+    sprite.setTextureRect(sf::IntRect(0*32, 0*32, 32, 32));
+    
+    // Lo dispongo en el centro de la pantalla
+    sprite.setPosition(posx, posy);
     
 
     //Bucle del juego
@@ -37,15 +64,85 @@ int main()
                     
                 //Se pulsó una tecla, imprimo su codigo
                 case sf::Event::KeyPressed:
-                    p1.movimiento_controlado(event,window);
-                    break;              
-                 
+                    
+                    //Verifico si se pulsa alguna tecla de movimiento
+                    switch(event.key.code) {
+                        
+                        //Mapeo del cursor
+                        case sf::Keyboard::Right:
+                            sprite.setTextureRect(sf::IntRect(1*32, 0*32, 32, 32));
+                            //Escala por defecto
+                            sprite.setScale(1,1);
+                            dirx = 1;
+                            diry = 0;
+                        break;
+
+                        case sf::Keyboard::Left:
+                            sprite.setTextureRect(sf::IntRect(1*32, 0*32, 32, 32));
+                            //Reflejo vertical
+                            sprite.setScale(-1,1);
+                            dirx = -1;
+                            diry = 0;
+                        break;
+                        
+                        case sf::Keyboard::Up:
+                            sprite.setTextureRect(sf::IntRect(0*32, 0*32, 32, 32));
+                            dirx = 0;
+                            diry = -1; 
+                        break;
+                        
+                        case sf::Keyboard::Down:
+                            sprite.setTextureRect(sf::IntRect(0*32, 0*32, 32, 32));
+                            sprite.setScale(1,-1);
+                            dirx = 0;
+                            diry = 1;  
+                        break;
+                        
+                        //Tecla ESC para salir
+                        case sf::Keyboard::Escape:
+                            window.close();
+                        break;
+                        
+                        //Cualquier tecla desconocida se imprime por pantalla su código
+                        default:
+                            std::cout << event.key.code << std::endl;
+                        break;
+                              
+                    }
+
             }
             
         }
+        mapa[(int)posx][(int)posy] = 1;
+        mapa[(int)posx][(int)posy] = 1;
         
+        
+        posx = posx + kVel * dirx;
+        posy = posy + kVel * diry;
+        
+        if(posx < 0){ posx = 600; }
+        if(posx > 600){ posx = 0; }
+        if(posy < 0){ posy = 480; }
+        if(posy > 480){ posy = 0; }
+        
+        sprite.setPosition(posx,posy);
+
         window.clear();
-        window.draw(p1.getSprite());
+        window.draw(s_background);
+        
+        for(int mx = 0; mx < 600;mx++){
+            for(int my = 0; my < 480;my++){
+                if(mapa[mx][my] == 1){
+                    sf::CircleShape rastro(2.f);
+                    rastro.setFillColor(sf::Color::Cyan);
+                    rastro.setOrigin(2.f,2.f);
+                    rastro.setPosition(mx,my);
+                    window.draw(rastro);
+                }
+            }
+        }
+        
+        window.draw(sprite);  
         window.display();
     }
 
