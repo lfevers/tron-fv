5b2a82880a24be88d901dae473013cea4b6f9f66
Unknown <sergiopatohumanes@gmail.com>
2018-02-22 11:31:15 +0100

    El rastro funciona ^^
    El coche se mueve bien , con tiempo constante y el rastro funciona

--- Tron-fv/main.cpp
+++ Tron-fv/main.cpp
@@ -1,31 +1,48 @@
 #include <iostream>
+#include <unistd.h>
 #include <SFML/Graphics.hpp>
 
-#define kVel 0
+#define W 600
+#define H 480
+#define kVel 4
 
 int main()
 {
-    float posx =300;
-    float posy =240;
+    sf::Clock clock;
+    
+    float posx =W/2;
+    float posy =H/2;
+    
+    int posr_x;
+    int posr_y;
     
     int dirx = 0;
-    int diry = 1;
+    int diry = -1;
     
-    int mapa[600][480];
-    for(int mx = 0; mx < 600;mx++){
-            for(int my = 0; my < 480;my++){
+    int mapa[W][H];
+    for(int mx = 0; mx < W;mx++){
+            for(int my = 0; my < H;my++){
                 mapa[mx][my] = 0;
             }
     }
     
+    sf::RectangleShape mapa_rastro[W/4][H/4];
+    for(int mx = 0; mx < W/4;mx++){
+            for(int my = 0; my < H/4;my++){
+                mapa_rastro[mx][my] = sf::RectangleShape(sf::Vector2f(4, 4));
+                mapa_rastro[mx][my].setFillColor(sf::Color::Cyan);
+                mapa_rastro[mx][my].setOrigin(2,2);
+                mapa_rastro[mx][my].setPosition(mx*4,my*4);
+            }
+    }
     
     //Creamos una ventana 
-    sf::RenderWindow window(sf::VideoMode(600, 480), "T.R.O.N");
+    sf::RenderWindow window(sf::VideoMode(W, H), "T.R.O.N");
     
     
     //Cargo la imagen donde reside la textura del sprite
     sf::Texture tex;
-    if (!tex.loadFromFile("resources/sprites-tron-sf.png"))
+    if (!tex.loadFromFile("resources/sprites-tron-sf2.png"))
     {
         std::cerr << "Error cargando la imagen sprites.png";
         exit(0);
@@ -51,6 +68,7 @@ int main()
     //Bucle del juego
     while (window.isOpen())
     {
+        clock.restart();
         //Bucle de obtención de eventos
         sf::Event event;
         while (window.pollEvent(event))
@@ -114,17 +132,15 @@ int main()
             }
             
         }
-        mapa[(int)posx][(int)posy] = 1;
-        mapa[(int)posx][(int)posy] = 1;
-        
-        
         posx = posx + kVel * dirx;
         posy = posy + kVel * diry;
         
-        if(posx < 0){ posx = 600; }
-        if(posx > 600){ posx = 0; }
-        if(posy < 0){ posy = 480; }
-        if(posy > 480){ posy = 0; }
+        mapa[(int)posx][(int)posy] = 1;
+
+        if(posx < 0){ posx = W; }
+        if(posx > W){ posx = 0; }
+        if(posy < 0){ posy = H; }
+        if(posy > H){ posy = 0; }
         
         sprite.setPosition(posx,posy);
 
@@ -132,10 +148,10 @@ int main()
         window.draw(s_background);
         
         
-         for(int mx = 0; mx < 600;mx++){
-            for(int my = 0; my < 480;my++){
+         for(int mx = 0; mx < W;mx++){
+            for(int my = 0; my < H;my++){
                 if(mapa[mx][my] == 1){
-                    //window.draw(mapa_rastro[mx][my]);
+                    window.draw(mapa_rastro[mx/4][my/4]);
                 }
             }
         }
@@ -143,7 +159,15 @@ int main()
         
         window.draw(sprite);  
         window.display();
+        
+        
+        sf::Time tiempo = sf::milliseconds(0);
+        sf::Time tiempo_max = sf::milliseconds(30);
+        while (tiempo < tiempo_max){
+            tiempo = clock.getElapsedTime();
+        }
+       
     }
-
+    
     return 0;
 }
